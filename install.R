13f277f03cb7bc5fef757dd3fb1b657cdbf6c5b8
Chi Po Choi <cpchoi@ucdavis.edu>
2021-04-27 01:42:04 -0700

    lab5 update

--- install.R
+++ install.R
@@ -1,6 +1,8 @@
+install.packages("devtools")
 install.packages("tidyverse")
 install.packages("rmarkdown")
 install.packages("formatR")
 tinytex::install_tinytex()
 install.packages("swirl")
 swirl::install_course_github("swirldev", "R_Programming_E")
+devtools::install_github("hadley/emo")
